5164594a2abec9290f683b8cc3d4b4a38007a37a
kshanta2 <kumar1.shantanu@hotmail.com>
2020-03-22 20:55:41 +0530

    added put patch and delete method

---
+++ profiles_api/serializers.py
@@ -0,0 +1,5 @@
+from rest_framework import serializers
+
+class HelloSerializer(serializers.Serializer):
+    """Serialzers a name field for testing our api view"""
+    name = serializers.CharField(max_length=10)
